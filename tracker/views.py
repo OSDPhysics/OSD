4aef91fb0f55397b3b6af7271f471219165f389c
James Wright <wright.j@gardenschool.edu.my>
2018-02-07 14:24:05 +0800

    Didn't commit properly earlier.

--- tracker/views.py
+++ tracker/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def front_login(request):
-    return render(request, 'tracker/front_login.html', {})  
+    return render(request, 'tracker/front_login.html', {})
+
+def teachers(request):
+    return render(request, 'tracker/teachers.html', {})
