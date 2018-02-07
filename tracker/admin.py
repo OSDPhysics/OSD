39ba7b1c56f33f0c5170bb4d0e8338e80edd548e
James Wright <wright.j@gardenschool.edu.my>
2018-02-07 17:05:34 +0800

    Added students to the DB

--- tracker/admin.py
+++ tracker/admin.py
@@ -3,8 +3,11 @@ from django.contrib import admin
 # Register your models here.
 
 from .models import Teacher
-from .models import Group
-
+from .models import TutorGroup
+from .models import ClassGroup
+from. models import Student
 
 admin.site.register(Teacher)
-admin.site.register(Group)
+admin.site.register(TutorGroup)
+admin.site.register(ClassGroup)
+admin.site.register(Student)
--- tracker/models.py
+++ tracker/models.py
@@ -32,9 +32,43 @@ class Teacher(models.Model):
     def save_user_profile(sender, instance, **kwargs):
         instance.teacher.save()
 
-class Group(models.Model):
+class ClassGroup(models.Model):
     groupname = models.CharField(max_length=50)
     groupteacher = models.ForeignKey('tracker.Teacher',on_delete=models.CASCADE)
 
     def __str__(self):
         return self.groupname
+
+class TutorGroup(models.Model):
+    tgname = models.CharField(max_length=5)
+    tgtutor = models.ForeignKey('tracker.Teacher',on_delete=models.CASCADE, blank=True)
+
+    def __str__(self):
+        return self.tgname
+
+class Student(models.Model):
+    GENDER = (
+            ('Male', 'Male'),
+            ('Female', 'Female'),
+            ('Undisclosed', 'Undisclosed'),
+        )
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    Gender = models.CharField(max_length=20, choices=GENDER, blank=True)
+    idnumber = models.IntegerField(blank=True,null=True)
+    tg = models.ForeignKey('tracker.TutorGroup',blank=True, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        space = ' '
+        fullname =  self.user.first_name + space + self.user.last_name #+ space + '(' + self.tg.tgname + ')'
+        #fullname = 'temporary'
+        return fullname
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Student.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.student.save()
