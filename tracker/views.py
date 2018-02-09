40518deab84a8be97a2058e0c1401e07cdacd2a1
James Wright <wright.j@gardenschool.edu.my>
2018-02-09 14:34:05 +0800

    xtable teachers
    Added an editable view to see all teachers listed in the database.
    Fixed error with all users being requried to be listed as both teachers and students.

--- tracker/views.py
+++ tracker/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+from .models import Teacher
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
 def front_login(request):
     return render(request, 'tracker/front_login.html', {})
 
+@login_required
 def teachers(request):
-    return render(request, 'tracker/teachers.html', {})
+    teachers = Teacher.objects.order_by('user__last_name')
+
+    return render(request, 'tracker/teachers.html', {'teachers': teachers})
