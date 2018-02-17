f3b162a56c96d34c076a94553396239b1aac47f8
James Wright <wright.j@gardenschool.edu.my>
2018-02-17 18:24:45 +0800

    Exams
    added in exams, with support for syllabuses, spec points and exam board.

--- tracker/admin.py
+++ tracker/admin.py
@@ -6,8 +6,18 @@ from .models import Teacher
 from .models import TutorGroup
 from .models import ClassGroup
 from. models import Student
+from .models import Exam
+from .models import Question
+from .models import *
+
 
 admin.site.register(Teacher)
 admin.site.register(TutorGroup)
 admin.site.register(ClassGroup)
 admin.site.register(Student)
+admin.site.register(Exam)
+admin.site.register(Question)
+admin.site.register(Examboard)
+admin.site.register(Examlevel)
+admin.site.register(Syllabus)
+admin.site.register(Syllabuspoint)
