c4b69b938091b2821a2b2d18824657eeed8e4ff4
jdjwright <35830787+jdjwright@users.noreply.github.com>
2018-03-16 16:15:24 +0800

    Delete urls.py

--- tracker/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from django.contrib import admin
-from django.contrib.auth import views as auth_views
-from . import views
-
-urlpatterns = [
-    path('exams/new_exam', views.add_test1, name='new_exam1'),
-]
