04987162020fa389023a4a65e84d77f30bc0240f
James Wright <wright.j@gardenschool.edu.my>
2018-08-17 14:28:28 +0800

    Second phase - use this to delete the date property from lessons.

--- timetable/models.py
+++ timetable/models.py
@@ -43,7 +43,6 @@ class TimetabledLesson(models.Model):
 
 
 class Lesson(models.Model):
-    date = models.DateField()
     lesson = models.ForeignKey(TimetabledLesson, on_delete=models.CASCADE)
     status = models.CharField(max_length=20, null=True, blank=True)
     syllabus_points_covered = models.ManyToManyField(SyllabusPoint)
