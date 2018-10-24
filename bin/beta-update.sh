e3167c3b79d0a6ded7554a522724e7cfcc65bdc8
James Wright <wright.j@gardenschool.edu.my>
2018-10-24 17:54:04 +0800

    Merge branch 'beta' of /Users/wright.j/PycharmProjects/OSD with conflicts.

---
+++ bin/beta-update.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+beta_directory = /home/james/OSD-beta/OSD
+backup_directory = /home/james/backups/OSD-beta-upgrade
+current_date = date +%Y%m%d%H%M%S
+
+# Stop the server
+sudo systemctl gunicorn stop
+
+# Backup the existing database
+python ${beta_directory}/manage.py dumpdata --natural-primary > ${backup_directory}/${current_data}.json
+
+# pull the database
+
+sudo git pull
+
+# check that the pull worked properly
+
+if [ $? -ne 1 ]
+then
+
+    sudo git shelve
+    sudo git pull
+    sudo git shelve apply
+
+else
+    echo Pull complete
+
+fi
+
