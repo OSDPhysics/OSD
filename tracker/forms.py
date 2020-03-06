0da2c0f7df5033d049740181e93614c53c7155fb
James Wright <jdjwright@gmail.com>
2020-03-06 12:06:00 +0800

    Updated css to improve select2 rendering

--- tracker/forms.py
+++ tracker/forms.py
@@ -8,8 +8,6 @@ from django.forms import formset_factory
 from searchableselect.widgets import SearchableSelect
 
 
-
-
 class CSVDetailsForm(forms.Form):
     topic = forms.ModelChoiceField(label='Topic', queryset=SyllabusTopic.objects.all())
 
@@ -17,7 +15,7 @@ class CSVDetailsForm(forms.Form):
 class CSVDocForm(forms.ModelForm):
     class Meta:
         model = CSVDoc
-        fields = ('description', 'document', )
+        fields = ('description', 'document',)
 
 
 class questionsForm(forms.ModelForm):
@@ -25,7 +23,8 @@ class questionsForm(forms.ModelForm):
         model = Question
         exclude = ('',)
         widgets = {
-            'syllabuspoint': SearchableSelect(model='tracker.SyllabusPoint', search_field='syllabusText', many=True, limit=10)
+            'syllabuspoint': SearchableSelect(model='tracker.SyllabusPoint', search_field='syllabusText', many=True,
+                                              limit=10)
         }
 
 
@@ -39,15 +38,17 @@ class NewExamForm(forms.ModelForm):
 
 
 class SetQuestions(forms.ModelForm):
-
     class Meta:
         model = Question
         fields = ['qorder', 'qnumber', 'maxscore', 'MPTTsyllabuspoint', 'exam']
         widgets = {
-             'MPTTsyllabuspoint': autocomplete.ModelSelect2Multiple(url='tracker:mptt_syllabus_autocomplete',
-                                                               forward=['points'],
-                                                               ),
-            'exam': forms.HiddenInput()
+            'MPTTsyllabuspoint': autocomplete.ModelSelect2Multiple(url='tracker:mptt_syllabus_autocomplete',
+                                                                   forward=['points'],
+                                                                   ),
+            'exam': forms.HiddenInput(),
+            'qorder': forms.NumberInput(attrs={'class': 'form-control'}),
+            'qnumber': forms.TextInput(attrs={'class': 'form-control'}),
+            'maxscore': forms.NumberInput(attrs={'class': 'form-control'}),
         }
 
     class Media:
@@ -57,27 +58,24 @@ class SetQuestions(forms.ModelForm):
 
 
 class SyllabusPoint(forms.ModelForm):
-
     class Meta:
         model = SyllabusPoint
         fields = ['topic', 'sub_topic', 'number', 'syllabusText']
 
-class NewSittingForm(forms.Form):
 
+class NewSittingForm(forms.Form):
     classgroup = forms.ModelChoiceField(ClassGroup.objects.all(),
                                         widget=autocomplete.ModelSelect2(url='tracker:classgroups-autocomplete'))
     date = forms.DateField(widget=forms.SelectDateWidget)
 
 
 class MarkForm(forms.ModelForm):
-
     class Meta:
         model = Mark
         fields = ['score']
 
 
 class MPTTSyllabusForm(forms.Form):
-
     parent = TreeNodeChoiceField(queryset=MPTTSyllabus.objects.all(), required=False)
 
 
@@ -97,12 +95,12 @@ class CustomModelChoiceField(TreeNodeMultipleChoiceField):
         if hasattr(self, '_choices'):
             return self._choices
         return CustomModelChoiceIterator(self)
+
     choices = property(_get_choices,
                        TreeNodeMultipleChoiceField._set_choices)
 
 
 class MPTTModelMultipleChoiceTree(forms.Form):
-
     points = TreeNodeMultipleChoiceField(queryset=MPTTSyllabus.objects.all(),
                                          widget=TreeSelectMultiple(attrs={'class': "syllabus-checkbox"}),
                                          level_indicator='')
@@ -110,6 +108,7 @@ class MPTTModelMultipleChoiceTree(forms.Form):
 
 class MPTTSelectMultipleField(TreeNodeMultipleChoiceField):
     widget = TreeSelectMultiple(attrs={'class': "syllabus-checkbox"})
+
     def _get_level_indicator(self, obj):
         return ''
 
